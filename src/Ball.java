07f6f6cb9ae722cc5d1aac5c1077abc4ffc755d3
Shea Polansky <env.laser@gmail.com>
2014-11-15 01:39:11 -0700

    Made Ball move and bounce

--- src/Ball.java
+++ src/Ball.java
@@ -1,3 +1,4 @@
+import java.awt.geom.Area;
 import java.awt.geom.Ellipse2D;
 
 /**
@@ -6,14 +7,56 @@ import java.awt.geom.Ellipse2D;
  * Bare-bones Ball object for breakout
  * Usage: None, used by Brooke's TestDriver class
  */
-public class Ball extends GameObject {
+public class Ball extends GameObject implements MovableObject {
+    private Ellipse2D.Double ellipse;
+    private int xSpeed = 0, ySpeed = 0;
+
     /**
      * Constructs a new Ball with given parameters
-     * @param x the leftmost x value of the ball
-     * @param y the uppermost y value of the ball
+     *
+     * @param x        the leftmost x value of the ball
+     * @param y        the uppermost y value of the ball
      * @param diameter the diameter of the ball
      */
     public Ball(int x, int y, int diameter) {
-        super(new Ellipse2D.Double(x, y, diameter, diameter));
+        super(null);
+        ellipse = new Ellipse2D.Double(x, y, diameter, diameter);
+    }
+
+    public int getXSpeed() {
+        return xSpeed;
+    }
+
+    public void setXSpeed(int xSpeed) {
+        this.xSpeed = xSpeed;
+    }
+
+    public int getYSpeed() {
+        return ySpeed;
+    }
+
+    public void setYSpeed(int ySpeed) {
+        this.ySpeed = ySpeed;
+    }
+
+    @Override
+    public Area getBoundingArea() {
+        return new Area(ellipse);
+    }
+
+    @Override
+    public void update(UpdateManager updateManager) {
+        int unclampedNewXPos = (int)ellipse.getX() + xSpeed;
+        int unclampedNewYPos = (int)ellipse.getY() + ySpeed;
+        int newXPos = Util.clamp(unclampedNewXPos, 0,
+                (int)(updateManager.getGameAreaWidth() - ellipse.getWidth()));
+        int newYPos = Util.clamp(unclampedNewYPos, 0,
+                (int)(updateManager.getGameAreaHeight() - ellipse.getHeight()));
+        if (newXPos != unclampedNewXPos) {
+            xSpeed *= -1;
+        }
+        if (newYPos != unclampedNewYPos) {
+            ySpeed *= -1;
+        }
     }
 }
