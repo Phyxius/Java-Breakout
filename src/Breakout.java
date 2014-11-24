3ab21ac166c127540da67ae41f077a47b321e3b8
Shea Polansky <env.laser@gmail.com>
2014-11-23 22:49:12 -0700

    Fixed threading properly

--- src/Breakout.java
+++ src/Breakout.java
@@ -1,31 +1,38 @@
+import javax.swing.*;
 import java.io.IOException;
 
 /**
  * Created by Shea on 11/14/2014.
  */
 public class Breakout {
+    private static String[] argsArray;
     public static void main(String[] args) {
-        //TODO: Do threading properly
+        argsArray = args;
+        SwingUtilities.invokeLater(Breakout::createAndShowGui);
+    }
+
+    private static void createAndShowGui() {
         Level level;
         try {
-            switch (args[0]) {
+            switch (argsArray[0]) {
                 case "fs":
-                    level = new FileSystemBasicFileLevel(args[1]);
+                    level = new FileSystemBasicFileLevel(argsArray[1]);
                     break;
                 case "jar":
-                    level = new JarFileBasicFileLevel(args[1]);
+                    level = new JarFileBasicFileLevel(argsArray[1]);
                     break;
                 default:
-                    throw new IOException();
+                    throw new IllegalArgumentException();
             }
         }
-        catch(IOException|ArrayIndexOutOfBoundsException e) {
+        catch(IOException|ArrayIndexOutOfBoundsException|
+                IllegalArgumentException e) {
             System.out.println(
                     "Loading level failed, falling back to internal level...");
             level = new BasicLevel(5, 13, 20, 20, 40, 20, 20, 20, 14);
         }
         new GameWindow(level,
-                (args.length > 2) && args[2].toLowerCase().equals("debug")
+                (argsArray.length > 2) && argsArray[2].toLowerCase().equals("debug")
         ).setVisible(true);
     }
 }
