bc4b70247ba3a2f4007360bfa4d7519f10ce1658
Shea Polansky <env.laser@gmail.com>
2014-11-23 20:50:17 -0700

    Renamed project properly

--- src/Breakout.java
+++ src/Breakout.java
@@ -1,10 +1,21 @@
+import java.io.IOException;
+import java.nio.file.Paths;
+
 /**
  * Created by Shea on 11/14/2014.
  */
 public class Breakout {
     public static void main(String[] args) {
         //TODO: Do threading properly
-        new GameWindow(new BasicLevel(5, 13, 20, 20, 40, 20, 20, 20, 14),
+        Level level;
+        try {
+            level = new FileSystemBasicFileLevel(Paths.get(
+                    "levels", "examples", "diamond.csv"));
+        }
+        catch(IOException e) {
+            level = new BasicLevel(5, 13, 20, 20, 40, 20, 20, 20, 14);
+        }
+        new GameWindow(level,
                 (args.length > 0) && args[0].toLowerCase().equals("debug")
         ).setVisible(true);
     }
--- src/FileSystemBasicFileLevel.java
+++ src/FileSystemBasicFileLevel.java
@@ -1,5 +1,6 @@
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.stream.Collectors;
 
@@ -7,8 +8,11 @@ import java.util.stream.Collectors;
  * Created by Phyxius on 11/23/2014.
  */
 public class FileSystemBasicFileLevel extends BasicFileLevel {
-    public FileSystemBasicFileLevel(String filePath) throws IOException {
-        super(Files.readAllLines(Paths.get(filePath)).stream().collect(
+    public FileSystemBasicFileLevel(Path filePath) throws IOException {
+        super(Files.readAllLines(filePath).stream().collect(
                 Collectors.joining("\n")));
+    }
+    public FileSystemBasicFileLevel(String filePath) throws IOException{
+        this(Paths.get(filePath));
     }
 }
