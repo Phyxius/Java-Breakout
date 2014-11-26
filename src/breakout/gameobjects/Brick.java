14c9a32514b944082701c6edf200c09fdd7c4a22
Shea Polansky <env.laser@gmail.com>
2014-11-26 15:57:14 -0700

    More brick commenting Add blank README

--- src/breakout/gameobjects/Brick.java
+++ src/breakout/gameobjects/Brick.java
@@ -18,7 +18,6 @@ public class Brick extends GameObject implements MovableObject {
     private static final float[] HIT_NUMBER_HUES = {
             0, 60f/360, 180f/360, 240f/360, 300f/360, 120f/360,
     };
-    @SuppressWarnings("FieldCanBeLocal")
     private final int RECTANGLE_SPACING = 2;
     private int hits;
     private final Rectangle rect;
@@ -31,6 +30,7 @@ public class Brick extends GameObject implements MovableObject {
      * @param width the width of the paddle
      * @param height the height of the paddle
      * @param hits the number of hits to destroy the brick, max 20
+     * @param value the score value of the brick
      */
     public Brick(int x, int y, int width, int height, int hits, int value) {
         if (hits > 20) {
@@ -41,35 +41,75 @@ public class Brick extends GameObject implements MovableObject {
         rect = new Rectangle(x, y, width, height);
         setBoundingArea(rect);
     }
+    /**
+     * Constructs a new Brick with given parameters
+     * @param x the leftmost x of the paddle
+     * @param y the uppermost y of the paddle
+     * @param width the width of the paddle
+     * @param height the height of the paddle
+     * @param hits the number of hits to destroy the brick, max 20
+     */
     public Brick(int x, int y, int width, int height, int hits) {
         this(x, y, width, height, hits, hits * 100);
     }
+
+    /**
+     * Constructs a new Brick with given parameters
+     * @param x the leftmost x of the paddle
+     * @param y the uppermost y of the paddle
+     * @param width the width of the paddle
+     * @param height the height of the paddle
+     */
     public Brick(int x, int y, int width, int height) {
         this(x, y, width, height, 1);
     }
 
+    /**
+     * @return the X speed
+     */
     public int getXSpeed() {
         return xSpeed;
     }
 
+    /**
+     * @param xSpeed the speed to set
+     */
     public void setXSpeed(int xSpeed) {
         this.xSpeed = xSpeed;
     }
 
+    /**
+     * @return the Y speed
+     */
     public int getYSpeed() {
         return ySpeed;
     }
 
+    /**
+     * @param ySpeed the speed to set
+     */
     public void setYSpeed(int ySpeed) {
         this.ySpeed = ySpeed;
     }
 
+    /**
+     * Moves the brick its speed (default 0)
+     * @param updateManager unused
+     */
     @Override
     public void update(UpdateManager updateManager) {
         rect.translate(getXSpeed(), getYSpeed());
         setBoundingArea(rect);
     }
 
+    /**
+     * Decrements the hit counter, destroying the brick if it is zero.
+     * Negative hit counters will not be changed or destroyed.
+     * 10 points are awarded for a decrement, or maxhits*100 if it is destroyed
+     * Invincible bricks award their worth every time
+     * @param other the GameObject that this one collided with
+     * @param manager the CollisionManager used to interact with the game world
+     */
     @Override
     public void onIntersect(GameObject other, CollisionManager manager) {
         if (other.getClass() == Ball.class) {
@@ -89,6 +129,15 @@ public class Brick extends GameObject implements MovableObject {
         }
     }
 
+    /**
+     * Draws the brick to the world. Bricks have 6 possible colors, and are
+     * drawn in layers, spaced according to RECTANGLE_SPACING. The outermost
+     * layer has 100% saturation, the next with 80%, and so on.
+     * All layers except the innermost are drawn with the last color.
+     * The innermost layer is indexed based on the number of hits.
+     * @param g the Graphics2D canvas to use
+     * @param manager the DrawingManager to use to interact with the game world
+     */
     @Override
     public void draw(Graphics2D g, DrawingManager manager) {
         int level = 0;
@@ -110,6 +159,9 @@ public class Brick extends GameObject implements MovableObject {
                 getHeight() - level * RECTANGLE_SPACING * 2);
     }
 
+    /**
+     * @return the brick's value
+     */
     public int getWorth() {
         return value;
     }
